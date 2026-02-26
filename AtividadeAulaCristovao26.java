8dfdf2aca5f387b1b78016bb0a204549e5b67276
Sarah <nunesdopradosarah@gmail.com>
2026-02-26 17:54:06 -0300

    Create AtividadeAulaCristovao26.java

---
+++ AtividadeAulaCristovao26.java
@@ -0,0 +1,16 @@
+public class DeclaracaoVariaveis {
+	
+	public static void main (String[] args) {
+		
+		String nome = "sarah";
+		String data = "27/06/2006";
+		String rg = "12345678";
+		String sexo = "Feminino";
+		double salario = 4000;
+		
+		
+		System.out.printf (" O Senhor(a) %s, portador(a) do RG %s, nascido em %s, do sexo %s, está registrado com o salário de R$ %f ", nome, rg, data, sexo, salario);
+			
+		
+	}
+}
