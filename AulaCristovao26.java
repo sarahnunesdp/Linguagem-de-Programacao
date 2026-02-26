5d48a211774a212c6657312b19853780c99444d8
Sarah <nunesdopradosarah@gmail.com>
2026-02-26 17:26:36 -0300

    Create AulaCristovao26.java

---
+++ AulaCristovao26.java
@@ -0,0 +1,25 @@
+public class DeclaracaoVariaveis {
+	
+	public static void main (String[] args) {
+		
+		byte b = 110;
+		short s = 1500;
+		int i = 1000000;
+		long l = 7l;
+		float f = 3.14f; // f pode ser maiusculo
+		double d =  3.14d; // ou double d = 3.14; , d pode ser maiusculo
+		char c = 's';
+		
+		boolean ok = true; // ou false
+		
+		String nome = "sarah"; // pode ser vazia
+		
+		System.out.print ();
+		
+		Systen.out,println (); //imprime e pula linha
+		
+		System.out.printf (); // imprime formatado
+		
+		
+	}
+}
