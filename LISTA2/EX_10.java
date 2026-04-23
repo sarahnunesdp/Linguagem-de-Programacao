a6e2fa34a010aed1f052aec6e593f8ddbd99dcb7
Sarah <nunesdopradosarah@gmail.com>
2026-04-23 15:57:11 -0300

    Create EX_10.java

---
+++ LISTA2/EX_10.java
@@ -0,0 +1,16 @@
+public class Armazenamento{
+	public static void main (String[]args){
+		
+		int numPedido = 01;
+		int cdProduto = 123;
+		int qtd = 3;
+		double valorTotal = 123.90;
+		
+		System.out.println("Dados da Compra");
+		System.out.println("Numero do pedido: " + numPedido);
+		System.out.println("Codigo do Pedido: " + cdProduto);
+		System.out.println("Quantidade: " + qtd);
+		System.out.println("Valor Total da Compra: " + valorTotal);
+    
+	}
+}
