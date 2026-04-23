70d4d49ace8664a7c3e7f9fc0d343d4047cbbc04
Sarah <nunesdopradosarah@gmail.com>
2026-04-23 16:27:56 -0300

    Create EX_02.java

---
+++ LISTA4/EX_02.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Nota{
+	public static void main (String[]args){
+		Scanner sc = new Scanner (System.in);
+		
+		double nota;
+		System.out.print("Insira a nota do aluno: ");
+		nota = sc.nextDouble ();
+		
+		if (nota<0 || nota>10){
+			System.out.print("Nota Invalida");
+		}
+		
+		else{
+			
+			if(nota<6){
+				System.out.print("Reprovado");}
+				
+				else{
+					System.out.print("Aprovado");}
+			
+		
+		}
+    
+	}
+
+}
+
