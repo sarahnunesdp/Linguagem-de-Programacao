06e7aca94fa31872f67d5f0b008f16111a6e61cf
Sarah <nunesdopradosarah@gmail.com>
2026-04-30 16:31:58 -0300

    Create EX_03.java

---
+++ LISTA5/EX_03.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Calcular{
+	public static void main (String[]args){
+		
+		Scanner sc = new Scanner(System.in);
+			int qtd;
+			double valorU, valorFinal;
+			
+			System.out.print("Digite a quantidade de maçãs: ");
+			qtd = sc.nextInt();
+			
+			if(qtd<12) {
+				valorU = 1.30;
+			}
+			else{
+				valorU = 1.00;
+				}
+			
+			valorFinal = qtd * valorU;
+			System.out.printf("Valor Final: %.2f", valorFinal);
+			
+		sc.close();
+		
+		
+		}
+	
+	}
