c2f788f665b10ee64298559d776bd1ce21f1ed84
Sarah <nunesdopradosarah@gmail.com>
2026-05-04 10:13:48 -0300

    Create EX_07.java

---
+++ LISTA5/EX_07.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ExercioLista {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n1,n2, maior, menor;
+        System.out.print("Digite o primeiro número: ");
+        n1 = sc.nextDouble();
+        System.out.print("Digite o segundo número: ");
+        n2 = sc.nextDouble();
+
+        if(n1>n2){
+            maior=n1;
+            menor=n2;
+        }
+        else{
+            maior = n2;
+            menor=n1;
+        }
+
+        System.out.printf ("Em ordem crescente: %.2f e %.2f", menor, maior);
+
+
+    }
+    
+}
