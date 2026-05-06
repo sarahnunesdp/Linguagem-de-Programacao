b17d6ff4d46a8636368665a47f89dd92af4b5da3
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 16:48:46 -0300

    Create EX_12.java

---
+++ LISTA_06/EX_12.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+	    int i;
+	    double n;
+	    double soma =0;
+	    
+	    for(i=0; i<10; i++){
+	        n=sc.nextDouble();
+	        soma = soma + n;
+	    }
+	    System.out.println("SOMA: " + soma);
+	    
+	    
+	   
+	}
+}
