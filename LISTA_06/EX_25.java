ec1c459b55ff96f847273250a62bc122366bc665
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 18:15:47 -0300

    Create EX_25.java

---
+++ LISTA_06/EX_25.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    int i;
+	    for(i=1; i<=10; i++){
+	        System.out.println(i + ", 1 2 3 4 5 6 7 8 9 10");
+	    }
+	}
+}
