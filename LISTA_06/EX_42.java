ec79e27b69f31666266b29904805fadcbe211e48
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 20:30:56 -0300

    Create EX_42.java

---
+++ LISTA_06/EX_42.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+	    int i;
+	    int fatorial=1;
+	    for(i=1; i<=10; i++){
+	        fatorial=fatorial*i;
+	        System.out.println(i+"!="+fatorial);
+	    }
+	    }
+	    
+    }
