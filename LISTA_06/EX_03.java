329931c205a6ae359740e5431628d3d1e29921c4
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 15:29:19 -0300

    Create EX_03.java

---
+++ LISTA_06/EX_03.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int x = 100;
+      int i;
+      
+      for(i=0; i<10; i++){
+        x++;
+        System.out.println(x);
+      }
+    }
+}
