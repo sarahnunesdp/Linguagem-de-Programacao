92d6afad46af8ce93a0ecfa9fe911f7df629f157
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 18:47:06 -0300

    Update EX_31.java

--- LISTA_06/EX_31.java
+++ LISTA_06/EX_31.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main
+{
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner (System.in);
 	    
 	    int i, qtd;
 	    
+	    do{
 	    System.out.print("Digite a quantidade de termos: ");
 	    qtd = sc.nextInt();
+	    } while(qtd<0);
 	    
-	    for(i=1; i<=qtd; i++){
-	        System.out.print(i+ " " );
-	    }
-	}
+	
+    	if(qtd==0){
+    	   System.out.println("Programa Finalizado");
+    	}
+    	else{
+    	   for (i=1; i<=qtd; i++){
+	       System.out.print(i+ " " );
+	            }
+    	    }
+    	        
+    	}
 }
