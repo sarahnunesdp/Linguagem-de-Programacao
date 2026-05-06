f5e35591f4ffebbfc414ad5d60f02b78ed282ce8
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 16:44:50 -0300

    Create EX_11.java

---
+++ LISTA_06/EX_11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+	   int alunos, i;
+	   double nota1, nota2, media;
+	   
+	   System.out.print("QUANTIDADE DE ALUNOS: ");
+	   alunos = sc.nextInt();
+	   
+	   for(i=0; i<alunos; i++){
+	       nota1 = sc.nextDouble();
+	       nota2 = sc.nextDouble();
+	       media= (nota1+nota2)/2;
+	       System.out.println("MEDIA: "+ media);
+	   }
+	}
+}
