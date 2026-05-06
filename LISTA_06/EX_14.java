034010dfae92dfa9291a8fae91ba73f2b3a5058d
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 17:06:45 -0300

    Create EX_14.java

---
+++ LISTA_06/EX_14.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+	    int x, y, i;
+	    int soma =0;
+	    
+	    
+	    x=sc.nextInt();
+	    y=sc.nextInt();
+	    
+	    for(i=x; i<=y; i++){
+	            soma = soma +i;
+	        
+	    }
+	    
+	    System.out.println("SOMA DOS INTEIROS: " + soma);
+	}
+}
