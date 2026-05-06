c928b6da7c1f3d054d6ab7f83ebf9cc8ea5a7108
Sarah <nunesdopradosarah@gmail.com>
2026-05-06 18:11:30 -0300

    Create EX_20.java

---
+++ LISTA_06/EX_20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner (System.in);
+	    
+	    int i, numero;
+	    int menor = 1000000;
+	    int maior = 0;
+	    for(i=0;i<100;i++){
+	        numero = sc.nextInt();
+	        if(numero>maior){
+	            maior=numero;
+	        }
+	        if(numero<menor){
+	            menor=numero;
+	        }
+	    }
+	    
+	    System.out.println("MAIOR: " + maior);
+	    System.out.println("MENOR: "+ menor);
+	}
+}
