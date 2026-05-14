f5c320af391d4b401b73caef745fb93b3662efa5
Sarah <nunesdopradosarah@gmail.com>
2026-05-14 16:55:42 -0300

    Create EX_01.java

---
+++ LISTA_07/EX_01.java
@@ -0,0 +1,15 @@
+/*
+ * LISTA 07, EX1
+ */
+
+
+public class Exercicio01 {
+	
+	public static void main (String[] args) {
+		
+		for(int i=0; i<256; i++) {
+			System.out.println(i + " - " + (char) i );
+		}
+		
+	}
+}
