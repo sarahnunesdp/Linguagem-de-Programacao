a4491c37946277eb9d38f0b1156a7529c75b310a
Sarah <nunesdopradosarah@gmail.com>
2026-05-28 17:37:34 -0300

    Create EX_10.java

---
+++ LISTA5/EX_10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+
+public class Salario {
+	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		double salarioFixo, valorVendas, salarioFinal, extra;
+		
+		System.out.println ("Digite o salário fixo: ");
+		salarioFixo = sc.nextDouble();
+		
+		System.out.println ("Digite o valor das vendas efetuadas");
+		valorVendas = sc.nextDouble();
+		
+		if(valorVendas <= 1500) {
+			extra = 0.3 * 1500;
+			salarioFinal = salarioFixo + extra;
+			}
+		else {
+			extra = 0.5*valorVendas;
+			salarioFinal = salarioFixo + extra;
+	}
+	
+	System.out.printf (" O salário final é de R$ %.2f ", salarioFinal);
+}
+}
+
