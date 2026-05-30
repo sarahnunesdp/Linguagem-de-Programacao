425189968d1664a38ef9d887fbbb0b26871b02da
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 15:59:31 -0300

    Create EX_02.java

---
+++ LISTA1/EX_02.java
@@ -0,0 +1,9 @@
+public class Argumentos{
+    public static void main(String[] args) {
+        String primeiro = "Hello";
+        String segundo = "World";
+        
+        System.out.println("Primeiro argumento: " + primeiro);
+        System.out.println("Segundo argumento: " + segundo);
+    }
+}
