d976184ebf197848eeffcd6def4e060c87269ebd
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:16:14 -0300

    Create EX_02.java

---
+++ LISTA2/EX_02.java
@@ -0,0 +1,10 @@
+//SARAH NUNES DO PRADO
+//L2EX02
+
+public class ExibeNumeroAleatorio {
+    public static void main(String [] args) {
+        
+        double numeroAleatorio = Math.random() * 100;
+        System.out.print(numeroAleatorio);
+    }
+}
