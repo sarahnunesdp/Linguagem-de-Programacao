3cb5779fbc1b145e98066c0187d057a25adb28c6
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 21:31:33 -0300

    Create EX_06.java

---
+++ LISTA1/EX_06.java
@@ -0,0 +1,17 @@
+// SARAH NUNES DO PRADO
+// L1EX06
+
+public class Cursos {
+    public static void main(String [] args) {
+        
+       System.out.println("F01");
+       System.out.println("\tLógica de Programação");
+       
+       System.out.println("F02");
+       System.out.println("\tLinguagem de Programação");
+       
+       System.out.println("F03");
+       System.out.println("\tProgramação Orientada a Objetos");
+       
+    }
+}
