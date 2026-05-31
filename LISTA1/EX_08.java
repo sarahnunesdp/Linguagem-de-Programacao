ab07f5a2c780cf6d7929bb6bfb1f01e4fd17761e
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 21:35:06 -0300

    Create EX_08.java

---
+++ LISTA1/EX_08.java
@@ -0,0 +1,11 @@
+// SARAH NUNES DO PRADO
+// L1EX08
+
+public class FrasePrefrida{
+    public static void main(String [] args) {
+        
+       System.out.println("Depois do medo vem o mundo");
+      
+       
+    }
+}
