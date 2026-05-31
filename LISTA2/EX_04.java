105fa9500dc8377a80cc443f2918bd67205202f7
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:26:50 -0300

    Create EX_04.java

---
+++ LISTA2/EX_04.java
@@ -0,0 +1,25 @@
+//SARAH NUNES DO PRADO
+//L2EX04
+
+public class ExibeValores {
+    public static void main(String [] args) {
+       
+        byte v1 = 19;
+        short v2 = 1571;
+        int v3 = 100000;
+        long v4 = 2147483648L; 
+        double v5 = 3.14;
+        double v6 = 3.141592653589793;
+        boolean v7 = true;
+        char v8 = 'K';
+
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+        System.out.println(v5);
+        System.out.println(v6);
+        System.out.println(v7);
+        System.out.println(v8);
+    }
+}
