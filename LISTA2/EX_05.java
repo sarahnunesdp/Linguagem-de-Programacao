66106c273964be572e54cf91b3eb4f259c6f246f
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:30:41 -0300

    Create EX_05.java

---
+++ LISTA2/EX_05.java
@@ -0,0 +1,13 @@
+//SARAH NUNES DO PRADO
+//L2EX05
+
+public class CopiaValores {
+    public static void main(String [] args) {
+        
+        float numero = (float) 3.14;
+        float numeroCopia = numero;
+        
+        System.out.println(numero);
+        System.out.print(numeroCopia);
+    }
+}
