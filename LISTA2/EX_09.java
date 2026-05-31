5952162da63e83cb78cc2e74a07271d9ddac5d2a
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:43:05 -0300

    Create EX_09.java

---
+++ LISTA2/EX_09.java
@@ -0,0 +1,25 @@
+//SARAH NUNES DO PRADO
+//L2EX09
+
+public class TestaTiposPrimitivos {
+    public static void main(String [] args) {
+    
+        byte by = 1;
+        short s = 100;
+        int i = 5000;
+        long l = 9999999999L;
+        float f =(float) 34.8;
+        double d = 134.98;
+        char c = 'S';
+        boolean b = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
