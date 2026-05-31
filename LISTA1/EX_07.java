ee21ae22cc5ab8aedf838b490059982ff117d404
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 21:33:43 -0300

    Create EX_07.java

---
+++ LISTA1/EX_07.java
@@ -0,0 +1,11 @@
+// SARAH NUNES DO PRADO
+// L1EX07
+
+public class DuasMensagens{
+    public static void main(String [] args) {
+        
+       System.out.println("Hello Worls 1");
+       System.out.println("Hello World 2");
+       
+    }
+}
