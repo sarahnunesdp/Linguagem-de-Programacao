0320cd79f0c7c523351e0474491991fe9f425af9
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 21:24:01 -0300

    Create EX_04.java

---
+++ LISTA1/EX_04.java
@@ -0,0 +1,11 @@
+// SARAH NUNES DO PRADO
+//L1EX4
+public class Triangulo{
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+}
