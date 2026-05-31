c9135b3feccc2e5308681cecbf92163d22767361
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:39:58 -0300

    Create EX_08.java

---
+++ LISTA2/EX_08.java
@@ -0,0 +1,11 @@
+//SARAH NUNES DO PRADO
+//L2EX08
+
+public class TestaVariavelPeso {
+    public static void main(String [] args) {
+        
+        double peso = 63;
+        System.out.println(peso);
+        System.out.printf("%.2f%n", peso);
+    }
+}
