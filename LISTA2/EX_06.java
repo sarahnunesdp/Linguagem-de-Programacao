fd6ad4ace62524481c4cba749c06177bd55d4f98
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 22:34:12 -0300

    deu erro

---
+++ LISTA2/EX_06.java
@@ -0,0 +1,14 @@
+//SARAH NUNES DO PRADO
+//L2EX06
+//OBS: nao compilou, deu erro
+
+public class ValorNull {
+    public static void main(String [] args) {
+        
+        Double numero = (Double) 3.14;
+        System.out.println(numero);
+        
+        numero = null;
+        System.out.println(numero);
+    }
+}
