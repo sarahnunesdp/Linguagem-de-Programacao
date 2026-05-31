e9843a74f1cf951db6886a8c536360d6bc2bb05b
Sarah <nunesdopradosarah@gmail.com>
2026-05-30 21:57:22 -0300

    Create EX_09.java

---
+++ LISTA1/EX_09.java
@@ -0,0 +1,12 @@
+// SARAH NUNES DO PRADO
+// L1EX09
+
+public class Fatec {
+    public static void main(String [] args) {
+        System.out.println("#####  ###  ##### #####  ###");
+        System.out.println("#     #   #   #   #      #   #");
+        System.out.println("####  #####   #   ####   #");
+        System.out.println("#     #   #   #   #      #   #");
+        System.out.println("#     #   #   #   #####  ###");
+    }
+}
