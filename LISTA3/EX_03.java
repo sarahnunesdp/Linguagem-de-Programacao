b4fe0b0c7708c39273d0c8a52e2a619f5b694999
Sarah <nunesdopradosarah@gmail.com>
2026-06-01 13:28:07 -0300

    Create EX_03.java

---
+++ LISTA3/EX_03.java
@@ -0,0 +1,13 @@
+//SARAH NUNES DO PRADO
+//L3EX03
+
+public class Conversao{
+    public static void main (String [] args){
+        
+        String texto = "19.09";
+        double numero = Double.parseDouble(texto);
+        
+        System.out.println ("Número: " + numero);
+        
+    }
+}
