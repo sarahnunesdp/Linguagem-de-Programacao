12b737f5adf9f502530a4e922b0763685ab58359
Sarah <nunesdopradosarah@gmail.com>
2026-06-01 13:22:30 -0300

    Create EX_02.java

---
+++ LISTA3/EX_02.java
@@ -0,0 +1,13 @@
+//SARAH NUNES DO PRADO
+//L3EX02
+
+public class Casting {
+    public static void main (String [] args){
+        
+        long numero1 = 2147483648L;
+        int numero2 = (int) numero1;
+        
+        System.out.println ("Número 1: " + numero1);
+        System.out.println ("Número 2: " + numero2);
+    }
+}
