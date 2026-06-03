7b56dc93484f1fe7aedccadf2f524064ca3050c6
Sarah <nunesdopradosarah@gmail.com>
2026-06-03 11:26:54 -0300

    Create EX_05.java

---
+++ LISTA3/EX_05.java
@@ -0,0 +1,16 @@
+//SARAH NUNES DO PRADO
+//L3EX05
+
+public class TiposDeResultado{
+    public static void main (String [] args){
+        byte a = 1;
+        short b = 2;
+        double c = 3.14;
+        
+        int d = (int) a+b;
+        double e = (double) b+c;
+        
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
