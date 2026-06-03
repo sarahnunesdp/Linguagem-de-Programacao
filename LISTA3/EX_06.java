3c876146d0b7a8728a848e7c43f27ec2847c219a
Sarah <nunesdopradosarah@gmail.com>
2026-06-03 11:36:58 -0300

    Create EX_06.java

---
+++ LISTA3/EX_06.java
@@ -0,0 +1,17 @@
+//SARAH NUNES DO PRADO
+//L3EX06
+
+public class DivisaoInteiraReal{
+    public static void main (String [] args){
+        
+        int a =5;
+        int b =2;
+        //2 - 2.5 - 2.5 -  2.5 - 2.0
+        
+        System.out.println (a/b);
+        System.out.println ((double) a/b);
+        System.out.println ((double) a / (double) b);
+        System.out.println (a / (double) b );
+        System.out.println ((double) (a/b));
+    }
+}
