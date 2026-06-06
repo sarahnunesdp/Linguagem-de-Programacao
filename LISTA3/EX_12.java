d5065c5e862676f002379515cb002ded838529a0
Sarah <nunesdopradosarah@gmail.com>
2026-06-06 11:15:06 -0300

    INCOMPLETO

---
+++ LISTA3/EX_12.java
@@ -0,0 +1,12 @@
+//SARAH NUNES DO PRADO
+//L3EX12
+
+public class OperadorTernario{
+    public static void main (String [] args){
+        
+        int a = (int) (Math.random() * 100);
+        int b = (int) (Math.random() * 100);
+        
+        System.out.println( a < b ? "a < b" : "a >= b" );
+    }
+}
