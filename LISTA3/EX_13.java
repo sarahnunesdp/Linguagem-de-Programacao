9b0447f5fdf1f70030b432b41176e608299a76ea
Sarah <nunesdopradosarah@gmail.com>
2026-06-06 11:17:53 -0300

    Create EX_13.java

---
+++ LISTA3/EX_13.java
@@ -0,0 +1,13 @@
+//SARAH NUNES DO PRADO
+//L3EX13
+
+public class OperadorNegacao{
+    public static void main (String [] args){
+        int a =10;
+        int b = 20;
+        
+        //true false
+        System.out.println (a<b);
+        System.out.println(!(a<b));
+    }
+}
