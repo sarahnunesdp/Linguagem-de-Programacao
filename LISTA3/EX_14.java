226dfdd2a7e689d7bc2aef5f4b869a26df6e450f
Sarah <nunesdopradosarah@gmail.com>
2026-06-06 11:37:00 -0300

    Create EX_14.java

---
+++ LISTA3/EX_14.java
@@ -0,0 +1,14 @@
+//SARAH NUNES DO PRADO
+//L3EX14
+
+public class PrePosIncrementoDecremento {
+    public static void main( String [] args) {
+        
+        int a = 1;
+
+        System.out.println(a++);
+        System.out.println(++a); 
+        System.out.println(a--); 
+        System.out.println(--a); 
+    }
+}
