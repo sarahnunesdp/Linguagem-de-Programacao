01ddc1c0b256bcabb03782e06b9560c28251f52f
Sarah <nunesdopradosarah@gmail.com>
2026-06-06 10:16:54 -0300

    Create EX_09.java

---
+++ LISTA3/EX_09.java
@@ -0,0 +1,28 @@
+public class OperadoresDeAtribuicao {
+    public static void main (String [] args) {
+        
+        int a=1; 
+        System.out.println(a); 
+
+        a=a+2; 
+        System.out.println(a); 
+
+        a=a-1; 
+        System.out.println(a); 
+
+        a=a*3; 
+        System.out.println(a);
+
+        a=a/2; 
+        System.out.println(a); 
+
+        a=a%2; 
+        System.out.println(a); 
+
+        a=a+1;
+        System.out.println(a); 
+
+        a=a-1; 
+        System.out.println(a); 
+    }
+}
